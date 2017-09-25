96092eb1056dec4d6c93c8a84025e6cd8ad93fef
bqk- <miclo.thibault@gmail.com>
2017-09-25 23:10:56 +0200

    another approach to CORS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,5 +17,16 @@ class AppServiceProvider extends ServiceProvider
             $app->configure('services');
             return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer');
         });
+        
+        $request = app('request');
+
+        if($request->getMethod() === 'OPTIONS')  {
+            app()->options($request->path(), function () {
+                return response('OK',200)
+                    ->header('Access-Control-Allow-Origin', '*')
+                    ->header('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, DELETE')
+                    ->header('Access-Control-Allow-Headers', 'Content-Type, Origin');                    
+            });
+        }
     }
 }
