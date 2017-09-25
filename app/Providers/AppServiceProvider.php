db49a64c52b1d84aaa7682db3e9ef858d389a128
bqk- <miclo.thibault@gmail.com>
2017-09-25 23:20:04 +0200

    yet another

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,16 +17,5 @@ class AppServiceProvider extends ServiceProvider
             $app->configure('services');
             return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer');
         });
-        
-        $request = app('request');
-
-        if($request->getMethod() === 'OPTIONS')  {
-            app()->options($request->path(), function () {
-                return response('OK',200)
-                    ->header('Access-Control-Allow-Origin', '*')
-                    ->header('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, DELETE')
-                    ->header('Access-Control-Allow-Headers', 'Content-Type, Origin');                    
-            });
-        }
     }
 }
